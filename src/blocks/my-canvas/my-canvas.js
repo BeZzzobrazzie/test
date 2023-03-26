ad2921fc9bf53eda3089d4013a45a56d26e9a2b2
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2023-03-26 20:38:59 +0300

    +

--- src/blocks/my-canvas/my-canvas.js
+++ src/blocks/my-canvas/my-canvas.js
@@ -5,13 +5,87 @@
 
 function draw() {
   let canvas = document.querySelector('.my-canvas');
+  let cHeight = canvas.getAttribute('height');
+  let cWidth = canvas.getAttribute('width');
+  
   let ctx = canvas.getContext('2d');
-  
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect (10, 10, 55, 50);
-  
-  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-  ctx.fillRect (30, 30, 55, 50);
+  let step = 20;
+  let currentPos = {x: 21, y: 21};
+
+  for (let i = step; i <= cWidth - step; i = i + step) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, cHeight);
+    ctx.stroke();
+    ctx.closePath();
+  }
+  for (let i = step; i <= cHeight - step; i = i + step) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(cWidth, i);
+    ctx.stroke();
+    ctx.closePath();
+  }  
+  ctx.fillStyle = 'green';
+  ctx.fillRect(21,21,18,18);
+
+  goTo(ctx, currentPos, step, cWidth, cHeight);
+
+  let i = new Creatures(0, 5, step);
+  i.create(ctx);
+
+}
+
+function goTo(ctx, currentPos, step, cWidth, cHeight) {
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyD' && currentPos.x <= cWidth - step) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(currentPos.x, currentPos.y, 18, 18);
+
+      ctx.fillStyle = 'green';
+      ctx.fillRect(currentPos.x + step, currentPos.y, 18, 18);
+      currentPos.x += step;
+    }
+    if (event.code == 'KeyA' && currentPos.x >= step) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(currentPos.x, currentPos.y, 18, 18);
+
+      ctx.fillStyle = 'green';
+      ctx.fillRect(currentPos.x - step, currentPos.y, 18, 18);
+      currentPos.x -= step;
+    }
+    if (event.code == 'KeyW' && currentPos.y >= step) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(currentPos.x, currentPos.y, 18, 18);
+
+      ctx.fillStyle = 'green';
+      ctx.fillRect(currentPos.x, currentPos.y - step, 18, 18);
+      currentPos.y -= step;
+    }
+    if (event.code == 'KeyS' && currentPos.y <= cHeight - step) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(currentPos.x, currentPos.y, 18, 18);
+
+      ctx.fillStyle = 'green';
+      ctx.fillRect(currentPos.x, currentPos.y + step, 18, 18);
+      currentPos.y += step;
+    }
+  })
+}
+
+
+class Creatures {
+  constructor(x, y, step) {
+    this.step = step;
+    this.currentPos = {x: this.step * x + 1, y: this.step * y + 1};
+  }
+
+
+  create(ctx) {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.currentPos.x, this.currentPos.y, 18, 18);
+  }
+
 }
 
 
